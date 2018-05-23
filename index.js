e91592b5080469404d076497a9b7830228287a4a
CodeBrew28 <arshiam2@illinois.edu>
2018-05-23 15:02:19 -0700

    add a team and get it

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const { send } = require('micro')
 const authenticate = mongoose => {
     const { Team } = require('schemas')(mongoose)
     return fn => async (req, res) => {
+        const newTeam = new Team({
+            name: "aria",
+            email: "aria@gmail.com"
+          })
+        await newTeam.save()
+      
+        const team = await Team.findOne({token}).exec()
+        console.log(team)
+        // console
+
         return send(res, 401, 'Unauthorized')
     }
 }
