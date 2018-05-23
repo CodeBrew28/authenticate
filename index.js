93ace57dbe1e942d372e0399102ce0f7e90a4167
CodeBrew28 <arshiam2@illinois.edu>
2018-05-23 16:13:10 -0700

    wip

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const authenticate = mongoose => {
     const { Team } = require('schemas')(mongoose)
     return fn => async (req, res) => {
         console.log("hi1")
-        const newTeam = new Team({
-            name: "aria",
-            email: "aria@gmail.com"
-          })
+        // const newTeam = new Team({
+        //     name: "aria",
+        //     email: "aria@gmail.com"
+        //   })
         await newTeam.save()
         console.log("hi2")
       
-        // const team = await Team.findOne({token}).exec()
+        // const team = await Team.findOne({name}).exec()
         // console.log(team)
         console.log("hi3")
         // console
